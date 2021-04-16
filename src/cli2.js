bec3aba4a9727385e2dad11ecc458a0ec93fd2a3
drkalach <kalach-2021@yandex.ru>
2021-04-16 21:13:05 +0300

    configure prettier.yaml

--- src/cli2.js
+++ src/cli2.js
@@ -1,35 +1,34 @@
-#!/usr/bin/env node
-
 import readlineSync from 'readline-sync';
 
 export default (play) => {
-  const welcomeString = 'Welcome to the Brain Games!';
-  console.log(welcomeString);
+    const welcomeString = 'Welcome to the Brain Games!';
+    console.log(welcomeString);
 
-  const userName = readlineSync.question('May I have your name?');
-  console.log(`Hello, ${userName}!`);
+    const userName = readlineSync.question('May I have your name?');
+    console.log(`Hello, ${userName}!`);
 
-  const description = 'Answer "yes" if the number is even, otherwise answer "no".';
-  console.log(description);
+    const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+    console.log(description);
 
-  function getRandomInRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  const randomNumberIs = getRandomInRange(-100, 100);
-
-  const game = (playerAnswer) => {
-    if ((playerAnswer === 'yes' && randomNumberIs % 2 === 0) || (playerAnswer === 'no' && randomNumberIs % 2 !== 0)) {
-      return console.log('Correct!');
-    } if (playerAnswer === 'yes' && randomNumberIs % 2 !== 0) {
-      return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
+    function getRandomInRange(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    if ((playerAnswer === 'no' && randomNumberIs % 2 === 0)) {
-      return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
-    }
-    return console.log(`It's wrong answer ;(. Correct answer was 'yes' or 'no'.\nLet's try again, ${userName}!`);
-  };
 
-  const firstNumber = readlineSync.question(`Question: ${randomNumberIs} \nYour answer: `);
-  game(firstNumber);
+    const randomNumberIs = getRandomInRange(-1000, 1000);
+
+    const game = (playerAnswer) => {
+        if ((playerAnswer === 'yes' && randomNumberIs % 2 === 0) || (playerAnswer === 'no' && randomNumberIs % 2 !== 0)) {
+            return console.log('Correct!');
+        }
+        if (playerAnswer === 'yes' && randomNumberIs % 2 !== 0) {
+            return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
+        }
+        if (playerAnswer === 'no' && randomNumberIs % 2 === 0) {
+            return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
+        }
+        return console.log(`It's wrong answer ;(. Correct answer was 'yes' or 'no'.\nLet's try again, ${userName}!`);
+    };
+
+    const firstNumber = readlineSync.question(`Question: ${randomNumberIs} \nYour answer: `);
+    game(firstNumber);
 };
