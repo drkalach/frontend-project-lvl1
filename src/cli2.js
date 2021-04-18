c12dd8ff5f663d3c72f2020a7665dcb04730a8c6
drkalach <kalach-2021@yandex.ru>
2021-04-18 10:14:42 +0300

    add 'test function' to make cycle of 'game function'

--- src/cli2.js
+++ src/cli2.js
@@ -14,22 +14,35 @@ export default (play) => {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    const firstRandomNumberIs = getRandomInRange(-1000, 1000);
+    const RandomNumberIs = getRandomInRange(1, 100);
 
-    const game1 = (playerAnswer) => {
-        if ((playerAnswer === 'yes' && firstRandomNumberIs % 2 === 0) || (playerAnswer === 'no' && firstRandomNumberIs % 2 !== 0)) {
-            return console.log('Correct!');
-        }
-        if (playerAnswer === 'yes' && firstRandomNumberIs % 2 !== 0) {
-            return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
-        }
-        if (playerAnswer === 'no' && firstRandomNumberIs % 2 === 0) {
-            return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
-        }
+    const game = () => {
+        const game1 = (playerAnswer) => {
+            if ((playerAnswer === 'yes' && RandomNumberIs % 2 === 0) || (playerAnswer === 'no' && RandomNumberIs % 2 !== 0)) {
+                return console.log('Correct!');
+            }
+            if (playerAnswer === 'yes' && RandomNumberIs % 2 !== 0) {
+                return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
+            }
+            if (playerAnswer === 'no' && RandomNumberIs % 2 === 0) {
+                return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
+            }
 
-        return console.log(`It's wrong answer ;(. Correct answer was 'yes' or 'no'.\nLet's try again, ${userName}!`);
+            return console.log(`It's wrong answer ;(. Correct answer was 'yes' or 'no'.\nLet's try again, ${userName}!`);
+        };
+
+        const Number = readlineSync.question(`Question: ${RandomNumberIs} \nYour answer: `);
+        game1(Number);
     };
 
-    const firstNumber = readlineSync.question(`Question: ${firstRandomNumberIs} \nYour answer: `);
-    game1(firstNumber);
-  };
+    const test = () => {
+        let result = game();
+
+        for (let i = 1; i < 3; i++) {
+            game();
+        }
+        return result;
+    };
+
+    test();
+};
